21529ba48a98798be0bc79f57d0498276bd92f62
Veronica <veronicajoanna30@gmail.com>
2024-10-30 18:53:48 +0100

    Uploaded

--- get_next_line.c
+++ get_next_line.c
@@ -5,44 +5,96 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmesa-ke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/25 13:37:29 by vmesa-ke          #+#    #+#             */
-/*   Updated: 2024/10/28 19:42:16 by vmesa-ke         ###   ########.fr       */
+/*   Created: 2024/10/30 17:38:08 by vmesa-ke          #+#    #+#             */
+/*   Updated: 2024/10/30 18:53:28 by vmesa-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <fcntl.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 20
+#endif
+
+char	*ft_strjoin(char *new_save, char *save)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)malloc (ft_strlen(new_save) + ft_strlen(save) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (*save)
+	{
+		save[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2)
+	{
+		save[i] = *new_
+	}
+}
+
+char	*ft_strchr(char *save, int chr)
+{
+	while (*s != '\0')
+	{
+		if ((unsigned char)*save == (unsigned char)chr)
+			return ((char *)save);
+		save++;
+	}
+	return (NULL);
+}
+
+char	*read_buffer(int fd, char *read)
+{
+	int		b_read;
+	char	*new_save;
+	
+	b_read = 1;
+	if (save == NULL)
+	{
+		read = malloc(1);
+		if (!read)
+			return (NULL);
+	}
+	while (!ft_strchr(read, '\n'))
+	{
+		b_read = read(fd, read, BUFFER_SIZE);
+		if (b_read <= 0)
+			return (NULL);
+		new_save = ft_strjoin(new_save, save);
+	}
+	return(save);
+	
+}
 
 char	*get_next_line(int fd)
 {
-	static char	*save;
-	char		*next_line;
+	//char	*line;
+	char	*save;
+	char	*read;
 
-	if (fd <= 0)
+	if (!fd)
 		return (NULL);
-	save = read_buffer(save, fd);
+	save = read_buffer(fd, read);
+	return (save);
 }
 
-char	*read_buffer(char *save, int fd)
+int main(void)
 {
-	char	*tmp;
-	int		b_read;
-	int		count;
+	char *prueba;
+	int	fd;
 
-	if (!save)
-		save = ft_calloc(1, 1);
-	tmp = ft_calloc(BUFFER_SIZE + 1, 1);
-	if (!tmp)
-		return (NULL);
-	b_read = 1;
-	while (b_read > 0)
-	{
-		b_read = read(fd, tmp, BUFFER_SIZE);
-		if (b_read <=0)
-			return (NULL);
-		save = ft_strjoin(save, tmp);
-		if (ft_strchr(save, '\n'))
-			break ;
-	}
+	fd = open("Hola.txt", O_RDONLY);
+	if (fd < 0)
+		return (1);
+	prueba = get_next_line(fd);
+	printf("1a linea encontrada: %s \n", prueba);
+	close(fd);
+	return (0);
 }
