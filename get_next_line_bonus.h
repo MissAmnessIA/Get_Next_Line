e261df447cf2f6c13cb5703e56d3273b0012d35d
Veronica Joanna Mesa Kenig <vmesa-ke@c2r16s6.42madrid.com>
2024-12-09 17:19:23 +0100

    Uploaded

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmesa-ke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 16:06:22 by vmesa-ke          #+#    #+#             */
-/*   Updated: 2024/11/15 16:06:26 by vmesa-ke         ###   ########.fr       */
+/*   Created: 2024/11/10 15:14:43 by vmesa-ke          #+#    #+#             */
+/*   Updated: 2024/11/13 17:30:22 by vmesa-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
-
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 100000000
 # endif
 
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdio.h>
 
 char	*get_next_line(int fd);
 char	*read_fd(int fd, char *saved);
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,15 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmesa-ke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 16:05:45 by vmesa-ke          #+#    #+#             */
-/*   Updated: 2024/11/15 16:05:50 by vmesa-ke         ###   ########.fr       */
+/*   Created: 2024/11/05 14:33:05 by vmesa-ke          #+#    #+#             */
+/*   Updated: 2024/11/13 17:21:59 by vmesa-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -26,10 +26,9 @@ char	*lost_chars(char	*saved)
 {
 	char	*new_save;
 	int		i;
-	char	*ptr;
 
 	i = 0;
-	ptr = saved;
+	char *ptr = saved;
 	while (*ptr != '\n')
 		ptr++;
 	ptr++;
@@ -37,7 +36,7 @@ char	*lost_chars(char	*saved)
 		i++;
 	new_save = (char *)malloc(i + 1);
 	if (!new_save)
-		return (NULL);
+		return (free(saved), NULL);
 	i = 0;
 	while (*ptr)
 	{
@@ -53,9 +52,11 @@ char	*lost_chars(char	*saved)
 char	*ft_strjoin(char *saved, char *readed)
 {
 	char	*joined;
+	char *temp;
 	int		i;
 
 	i = 0;
+	temp = saved;
 	joined = (char *)malloc(ft_strlen(saved) + ft_strlen(readed) + 1);
 	while (*saved)
 	{
